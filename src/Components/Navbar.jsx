2631d8842dcddd8e3ef7f228c3e5ea3b970b1e97
alexistorosina1 <alexistorosina1@gmail.com>
2025-10-20 13:48:02 -0400

    intial commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <ul>
+                <li>Menu</li>
+                <li>Contact</li>
+                <li>About Us</li>
+            </ul>
+        </nav>
+    )
+}
