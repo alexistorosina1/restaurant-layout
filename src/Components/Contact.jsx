f823ac397893b3fe3b49dbb3acabb53c51a93cc5
alexistorosina1 <alexistorosina1@gmail.com>
2025-10-22 15:19:34 -0400

    added google maps location of the restaurant to the contact card

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,21 @@
 export default function Contact(){
     return(
         <section>
-            <div>
-                hello
+            <div className="contact-container">
+                <article>
+                    <iframe
+                        title="Sabores de mi tierra location" 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.727262002916!2d-73.92437439999999!3d40.702003999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25d38e548fca5%3A0xe676002828711bf9!2sSabores%20de%20mi%20tierra!5e0!3m2!1sen!2sus!4v1761160523297!5m2!1sen!2sus" 
+                        width="600" 
+                        height="450" 
+                         
+                        allowfullscreen="" 
+                        loading="lazy" 
+                        referrerpolicy="no-referrer-when-downgrade" />
+                </article>
+                <article>
+
+                </article>
             </div>
         </section>
     )
